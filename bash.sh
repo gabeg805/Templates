1d082ff861a7e7da92b8741f2003476b2b20f3d0
Gabriel Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-06-27 15:44:14 -0400

    Updated script template to most recent convention.
    - No dependencies.
    - Define project name, files/directories, options, exit statuses all globally at
      top of script.
    - Parse objects using getopt and run options afterwards.
    - Options are primarily meant to be flags or data arrays in order to execute
      their respective functions.

--- bash.sh
+++ bash.sh
@@ -3,106 +3,87 @@
 # 
 # Name:    NAME
 # Author:  Gabriel Gonzalez
-# Email:   gabeg@bu.edu
-# License: The MIT License (MIT)
 # 
-# Syntax: SYNTAX
-# 
-# Description: DESCRIPTION
-# 
-# Notes: NOTES
+# Brief: DESCRIPTION
 # 
 # ------------------------------------------------------------------------------
 
-# Globals
-PROG="${0##*/}"
-PROGDIR=$(dirname "$(readlink -f "${0}")")
 
-# Options
-VERBOSE=
+##
+# Project name.
+##
+PROJECT="${0##*/}"
 
-# Source utility
-. "${PROGDIR}/../lib/bash/util.sh"
+##
+# Files/directories.
+##
+FILE=
 
-# Exit statuses
-# EACTION=10
+##
+# Options.
+##
+OPTION=
 
-# ------------------------------------------------------------------------------
-# Main
+##
+# Exit statuses.
+##
+EXIT_PROJECT_FUNCTION_ERROR=1
+
+##
+# Main.
+##
 main()
 {
-    # Setup options
-    if [ $# -eq 0 ]; then
-        usage
-        exit 0
-    fi
-
-    short="hv"
-    long="help,verbose"
-    args=$(getopt -o "${short}" --long "${long}" --name "${PROG}" -- "${@}")
-
+    # Parse options
+    local short="h"
+    local long="help"
+    local args=$(getopt -o "${short}" --long "${long}" \
+                        --name "${PROJECT}" -- "${@}")
     if [ $? -ne 0 ]; then
         usage
         exit 1
     fi
-
     eval set -- "${args}"
 
-    # Define options
-    local PARSED=
-
-    # Parse options
-    while true; do
+    while true
+    do
         case "${1}" in
-            # Usage
             -h|--help)
                 usage
-                exit 0
+                return 0
                 ;;
-
-            # Verbose output
-            -v|--verbose)
-                VERBOSE=true
-                ;;
-
-            # End of options
-            --)
+            *)
                 break
                 ;;
         esac
         shift
-        PARSED=true
     done
 
     # Run options
-    if [ -z "${PARSED}" ]; then
-        print_err "No options specified. See '--help' for more information."
-        exit ${EARG}
-    elif [ -n "${OPTION}" ]; then
-        # Execute option
+    if [ -n "${OPTION}" ]; then
         :
     else
         :
     fi
 }
 
-# ------------------------------------------------------------------------------
-# Print program usage
+##
+# Print program usage.
+##
 usage()
 {
-    echo "Usage: ${PROG} [options] [args]"
-    echo
+    echo "Usage: ${PROJECT} [options] [args]"
+    echo 
     echo "Options:"
     echo "    -h, --help"
     echo "        Print program usage."
-    echo
-    echo "    -v, --verbose"
-    echo "        Verbose output."
-    echo
+    echo 
     echo "Arguments:"
-    echo "    <arg>         Meaning of argument."
+    echo "    <arg>"
+    echo "        Meaning of argument."
 }
 
-# ------------------------------------------------------------------------------
-# Run script
+##
+# Run script.
+##
 main "${@}"
